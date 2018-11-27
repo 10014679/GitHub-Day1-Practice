4a75572cfdb37928eee6cd7a8fb3077a1a54a803
Skyla Qian <10014679@sbstudents.org>
2018-11-27 12:44:20 -0500

    Made ArrayLists
    I made the ArrayLists.

---
+++ GitHubDay1.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class GitHubDay1{
+	public static void main(String[]args){
+
+	ArrayList<Integer> list1 = new ArrayList<>();
+	ArrayList<Integer> list2 = new ArrayList<>();
+
+	}
+}
